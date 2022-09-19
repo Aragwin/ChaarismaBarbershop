758bf0a956c8048092e4b5b9c788021f59a2ea9a
Aragwin <102301019+Aragwin@users.noreply.github.com>
2022-09-19 21:20:44 +0200

    Update 0.2

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
+
+var myDate = new Date();
+var myDay = myDate.getDay();
+
+// Array of days.
+var weekday = ['Sonntag', 'Montag', 'Dienstag',
+    'Mittwoch', 'Donerstag', 'Freitag', 'Samstag'
+];
+
+// Array of closing time
+var closingtime = ['Heute Geschlossen', '19:00', '19:00',
+    '19:00', '19:00', '19:00', '18:00']
+document.getElementById("weekday[myDay]").innerHTML = weekday[myDay];
+document.getElementById("closingtime[myDay]").innerHTML = closingtime[myDay];
