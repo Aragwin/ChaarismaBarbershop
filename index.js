0178292411c4bc1f15914a4c8f95afc33780b4e7
Aragwin <102301019+Aragwin@users.noreply.github.com>
2022-09-22 15:53:56 +0200

    website update 0.3

--- index.js
+++ index.js
@@ -15,3 +15,20 @@ var closingtime = ['Heute Geschlossen', '19:00', '19:00',
     '19:00', '19:00', '19:00', '18:00']
 document.getElementById("weekday[myDay]").innerHTML = weekday[myDay];
 document.getElementById("closingtime[myDay]").innerHTML = closingtime[myDay];
+
+// SLiderShow
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}
+    slides[slideIndex-1].style.display = "block";
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
