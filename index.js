a43832b4d20ec2f14b30bd9e6b5859b5f885da29
Aragwin <102301019+Aragwin@users.noreply.github.com>
2022-09-30 14:21:31 +0200

    update 0.5

--- index.js
+++ index.js
@@ -18,17 +18,21 @@ document.getElementById("closingtime[myDay]").innerHTML = closingtime[myDay];
 
 // SLiderShow
 
-let slideIndex = 0;
+let slideIndex = 1;
 showSlides();
 
-function showSlides() {
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
     let i;
     let slides = document.getElementsByClassName("mySlides");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
     slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
